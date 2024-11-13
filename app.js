b2b4db4833d059a3e5ecff20e4d701234ef4b576
Svyazist <sviazist.vsa@gmail.com>
2024-11-13 19:40:42 +0500

    whot are fuck

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const app = express();
 const path = require('path');
 const fs = require('fs');
 const port = 3000;
+const base = 'pc';
 
+ 
 app.use(express.static(__dirname));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -14,9 +16,22 @@ app.get("/", (req, res) =>{
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.post('/',(req,res) => {
+    switch(req.body.os){
+        case "pc":
+            base = 'main_d.css';
+            break;
+        case "mobile":
+            base = 'main_m.css';
+            break;
+    }
+     
+    res.send(`<link rel="stylesheet" href="css/desctop/${base}.css">`)
+})
+
 const options = {
     key: fs.readFileSync('server.key'),
-    cert: fs.readFileSync('server.cert'),
+    cert: fs.readFileSync('server.cert'), 
 }
 
 https.createServer(options, app).listen(port, function (req, res){
--- scripts/start.js
+++ scripts/start.js
@@ -1,2 +1,38 @@
 "use strict"
 
+async function getOS() {
+
+    var userAgent = window.navigator.userAgent,
+        platform = window.navigator.platform,
+        macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+        windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+        iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+        os = null;
+  
+    if (macosPlatforms.indexOf(platform) !== -1) {
+      os = 'pc';
+    } else if (iosPlatforms.indexOf(platform) !== -1) {
+      os = 'mobile';
+    } else if (windowsPlatforms.indexOf(platform) !== -1) {
+      os = 'pc';
+    } else if (/Android/.test(userAgent)) {
+      os = 'mobile';
+    } else if (!os && /Linux/.test(platform)) {
+      os = 'pc';
+    }
+    alert(os);
+    let response = await fetch("https://localhost:3000",{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'text/plain'
+        },
+        body: os,
+    });
+
+    let stylesheet = await response.text();
+    alert(stylesheet);
+    document.querySelector('head').append(stylesheet);
+
+    
+  }
+  
