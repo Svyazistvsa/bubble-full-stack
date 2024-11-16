c86542d69836af497f3fc8b10536eb8927641265
Svyazist <sviazist.vsa@gmail.com>
2024-11-16 22:05:10 +0500

    animation_interface_mobile.js is work

--- scripts/animation_interface_mobile.js
+++ scripts/animation_interface_mobile.js
@@ -1,11 +1,20 @@
 "use strict"
 
 let all = document.querySelector("#all");
+const allOut = document.querySelectorAll(".out");
 
-all.addEventListener("clicl", (e) => {
-    if(e.currentTarget.classList.contains("out")) e.currentTarget.classList.add("hov");
+allOut.forEach((item) => {
+    item.addEventListener("click", (e) => {
+        e.currentTarget.classList.add("hov");
+    })
 })
 
+
+
+//all.addEventListener("click", (e) => {
+//    if(e.target.classList.contains("out")) e.target.classList.add("hov");
+//}, true)
+
 all.addEventListener("animationend", (e) => {
-    if(e.animationName == "shake") e.currentTarget.classList.remove("hov");
+    if(e.animationName == "shake") e.target.classList.remove("hov");
 })
