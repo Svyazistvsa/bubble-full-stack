20d32f24cd329decfede4d97da8e555516f3769a
Svyazist <sviazist.vsa@gmail.com>
2024-12-11 22:09:40 +0500

    post request for content

--- scripts/main_data.js
+++ scripts/main_data.js
@@ -4,6 +4,10 @@ let nav = document.querySelector("nav");
 nav.addEventListener("pointerdown", async (e) => {
     if(e.target.hasAttribute("data-name")){
         let name = e.target.dataset.name;
-        await fetch("https://localhost300" + name);
+        let response = await fetch("https://localhost300", {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({name : e.target.dataset.name}),
+        });
     }
 })
