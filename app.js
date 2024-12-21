a6fd56a19f1ae3bfc3b57a053fc9c5ba1980334e
Svyazist <sviazist.vsa@gmail.com>
2024-12-21 18:54:29 +0500

    insert title content in 'min' tag

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ const cheerio = require('cheerio');
 const app = express();
 const port = 3000;
 
-let base = 'main_d.css';
-let main_arr = [`<li class="out main_content">Главная страница</li>`];
+let base = 'main_d.css',
+    main_arr = [`<li class="out main_content">Главная страница</li>`],
+    main_cont = [] ;
 
 
 async function readContentDirectory() {
@@ -17,18 +18,21 @@ async function readContentDirectory() {
         const dirPath = path.join(__dirname, 'content');
         await fs.access(dirPath); 
         const files = await fs.readdir(dirPath);
-        console.log('Содержимое директории:', files); 
         for (const elem of files) {
             const filePath = path.join(dirPath, elem);
             const stats = await fs.stat(filePath);
             if (stats.isDirectory()) {
-                console.log(`${elem} - это директория`);
                 continue;
             }
             const fileContent = await fs.readFile(filePath, 'utf-8');
             const $ = cheerio.load(fileContent);
             const zag = $("h1").text();
+            const img = $("img.main").attr("src");
             main_arr.push(`<li class="out" data-name="${elem}">${zag}</li>`);
+            main_cont.push([
+                `<li class="out" data-name="${elem}">${zag}</li>`,
+                `<img src="${path.join(__dirname,img)}" alt="${zag}>`
+            ]);
         }
         main_arr.push(`<li class="out relax ">Релакс</li>`);
     } catch (error) {
@@ -59,6 +63,8 @@ app.use('/css', express.static(path.join(__dirname, 'css')));
 app.get("/", (req, res) => {
     if (req.query.menu) {
         res.json(main_arr);
+    } if(req.query.main){
+        res.json(main_cont);
     } else {
         res.sendFile(path.join(__dirname, 'scout.html'));
     }
--- scripts/main_data.js
+++ scripts/main_data.js
@@ -1,8 +1,8 @@
-//import {list} from "./main_menu.js";
+"use strict"
 
 let nav = document.querySelector("nav"),
-    main = document.getElementsByTagName("main");
-    alert(main);
+    main = document.getElementsByTagName("main")[0];
+
 nav.addEventListener("pointerdown", async (e) => {
     if(e.target.hasAttribute("data-name")){
         let name = e.target.dataset.name;
@@ -14,10 +14,25 @@ nav.addEventListener("pointerdown", async (e) => {
 
         if (response.ok) {
             const newDocument = await response.text();
-            main[0].innerHTML = newDocument;
+            main.innerHTML = newDocument;
             
         } else {
             console.error('Ошибка при отправке запроса:', response.status);
         }
+    }
+    if(e.target.classList.contains("main_content")){
+        let response = await fetch("https://localhost:3000?main=main", {
+            method: 'GET',
+            headers: { "Content-Type": "application/json" },
+        });
+        if (response.ok) {
+            let content = await response.json(); 
+            alert(content);
+            main.innerHTML = content[0];
+            main 
+        } else {
+            console.log("Error download");
+            return []; 
+        }        
     }
 })
