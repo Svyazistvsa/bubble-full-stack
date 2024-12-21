9a86ff796e5351064c04e9e857754928a04144ed
Svyazist <sviazist.vsa@gmail.com>
2024-12-21 17:29:01 +0500

    insert content in 'main' tag

--- scripts/main_data.js
+++ scripts/main_data.js
@@ -1,6 +1,8 @@
 //import {list} from "./main_menu.js";
 
-let nav = document.querySelector("nav");
+let nav = document.querySelector("nav"),
+    main = document.getElementsByTagName("main");
+    alert(main);
 nav.addEventListener("pointerdown", async (e) => {
     if(e.target.hasAttribute("data-name")){
         let name = e.target.dataset.name;
@@ -12,9 +14,8 @@ nav.addEventListener("pointerdown", async (e) => {
 
         if (response.ok) {
             const newDocument = await response.text();
-            document.open();
-            document.write(newDocument);
-            document.close();
+            main[0].innerHTML = newDocument;
+            
         } else {
             console.error('Ошибка при отправке запроса:', response.status);
         }
