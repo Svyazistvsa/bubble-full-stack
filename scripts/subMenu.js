ebf15476ab7fffa6fc465dc683a7cb00b97e3e98
Svyazistvsa <sviazist.vsa@gmail.com>
2025-01-20 19:22:13 +0500

    get heads

--- scripts/subMenu.js
+++ scripts/subMenu.js
@@ -1,8 +1,12 @@
 "use strict"
 
 const content = document.getElementsByTagName("main")[0];
-let head = content.querySelectorAll("h1,h3,h4");
 
-alert(head[0]);
+
+document.addEventListener("newContent", () => {
+    let head = content.querySelectorAll("h1,h3,h4");
+    alert(`${head[0].innerHTML}`);    
+})
+
 
 
