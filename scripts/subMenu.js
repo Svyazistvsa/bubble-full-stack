31da1856953b5b19eb4bbeb7648e6f611ed7b404
Svyazistvsa <sviazist.vsa@gmail.com>
2025-01-22 17:31:46 +0500

    subMenu not work

--- scripts/subMenu.js
+++ scripts/subMenu.js
@@ -1,11 +1,18 @@
 "use strict"
 
 const content = document.getElementsByTagName("main")[0];
-
+const aside = document.getElementsByTagName("aside")[0];
+let ul = document.createElement("ul");
 
 document.addEventListener("newContent", () => {
-    let head = content.querySelectorAll("h1,h3,h4");
-    alert(`${head[0].innerHTML}`);    
+    let heads = content.querySelectorAll("H1");
+    heads.forEach((item, index, array) => {
+        let li = document.createElement("li");
+        li.textContent = item.text;
+        ul.append(li);
+        aside.append(ul);
+    })
+    
 })
 
 
