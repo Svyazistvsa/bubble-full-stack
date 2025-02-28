4b2dc82d9c8a96f398b5090256b673c7903ab8d1
Svyazistvsa <sviazist.vsa@gmail.com>
2025-02-28 19:30:36 +0500

    image full debages

--- scripts/image_full.js
+++ scripts/image_full.js
@@ -3,15 +3,19 @@
 document.addEventListener("newContent", () => {
     let main = document.querySelector("main"),
         allScreen = document.querySelector("#all");
-        alert(allScreen);
     
     main.addEventListener("pointerdown", (e) =>{
         let target = e.target;
         if(target.tagName == "IMG"){
-            let imgScreen = document.createElement("DIV");
+            let imgScreen = document.createElement("DIV"),
+                clone = target.cloneNode(true);
+            
             imgScreen.classList.add("fullImg");
-            imgScreen.append(target.cloneNode(true));
+            imgScreen.append(clone);
             allScreen.append(imgScreen);
+            for(let i of clone.classList){
+                clone.classList.remove(i);
+            }
             imgScreen.addEventListener("pointerdown", () => {
                 imgScreen.remove();
             })
