7878ad6a369d51109398d96196ef50851396ad53
Svyazistvsa <sviazist.vsa@gmail.com>
2025-02-28 20:03:27 +0500

    image_full.js is work

--- scripts/image_full.js
+++ scripts/image_full.js
@@ -1,25 +1,27 @@
 "use strict"
 
-document.addEventListener("newContent", () => {
-    let main = document.querySelector("main"),
+let main = document.querySelector("main"),
         allScreen = document.querySelector("#all");
-    
-    main.addEventListener("pointerdown", (e) =>{
-        let target = e.target;
-        if(target.tagName == "IMG"){
-            let imgScreen = document.createElement("DIV"),
-                clone = target.cloneNode(true);
-            
-            imgScreen.classList.add("fullImg");
-            imgScreen.append(clone);
-            allScreen.append(imgScreen);
-            for(let i of clone.classList){
-                clone.classList.remove(i);
-            }
-            imgScreen.addEventListener("pointerdown", () => {
-                imgScreen.remove();
-            })
-        }
-    })
 
+document.addEventListener("newContent", () => {    
+    main.removeEventListener("pointerdown", full);
+    main.addEventListener("pointerdown", full);
 })
+
+let full = (e) =>{
+    let target = e.target;
+    if(target.tagName == "IMG"){
+        let imgScreen = document.createElement("DIV"),
+            clone = target.cloneNode(true);
+        
+        imgScreen.classList.add("fullImg");
+        imgScreen.append(clone);
+        allScreen.append(imgScreen);
+        for(let i of clone.classList){
+            clone.classList.remove(i);
+        }
+        imgScreen.addEventListener("pointerdown", () => {
+            imgScreen.remove();                
+        })
+    }
+}
