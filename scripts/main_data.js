7eec5c4443b5f28b8461a92d78dcffabf7b4d62b
Svyazistvsa <sviazist.vsa@gmail.com>
2025-03-03 19:05:30 +0500

    hestory work back onli

--- scripts/main_data.js
+++ scripts/main_data.js
@@ -3,29 +3,45 @@
 let main = document.getElementsByTagName("main")[0],
     subMenu = document.querySelector("#subMenu");    
 
-document.addEventListener("pointerdown", async (e) => {   
+window.addEventListener("popstate", (e) => {
+    if(e.state){
+        switch(e.state.name){
+            case 'main':
+                res();
+                break;
+            default : 
+                contentF(e.state.name);            
+        }
+    }
+})
+
+document.addEventListener("pointerdown", (e) => {   
     if(e.target.hasAttribute("data-name")){
         let name = e.target.dataset.name;
-        let response = await fetch("https://localhost:3000/content", {
-            method: 'POST',
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({name : name}),
-        });
-
-        if (response.ok) {
-            const newDocument = await response.text();
-            main.innerHTML = newDocument;
-            if(subMenu.classList.contains("hidden")) subMenu.classList.remove("hidden");
-            document.dispatchEvent(new CustomEvent("newContent", {bubbles:true}));
-            history.pushState({name : name, path:"https://localhost:3000/content"}, "", "https://localhost:3000/content");
-        } else {
-            console.error('Ошибка при отправке запроса:', response.status);
-        }
+        contentF(name);        
     }
     if(e.target.classList.contains("main_content")){
         res();
     }
 })
+
+async function contentF(name){
+    let response = await fetch("https://localhost:3000/content", {
+        method: 'POST',
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({name : name}),
+    });
+
+    if (response.ok) {
+        const newDocument = await response.text();
+        main.innerHTML = newDocument;
+        if(subMenu.classList.contains("hidden")) subMenu.classList.remove("hidden");
+        document.dispatchEvent(new CustomEvent("newContent", {bubbles:true}));
+        history.pushState({name: name, path:"https://localhost:3000/content/"+name+"/" }, "", "https://localhost:3000/content/"+name+"/");
+    } else {
+        console.error('Ошибка при отправке запроса:', response.status);
+    }
+}
 
 let res = async () =>{
     let response = await fetch("https://localhost:3000?main=main", {
@@ -38,6 +54,7 @@ let res = async () =>{
         content.forEach((item) => {
             main.innerHTML +=item;
         })
+        history.pushState({name: "main", path:"https://localhost:3000"},"", "https://localhost:3000")
 
         subMenu.classList.add("hidden");
         if(document.querySelector(".subUl")) {document.querySelector(".subUl").classList.add("hidden")};
