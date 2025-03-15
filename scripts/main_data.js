0517269fce2db74ed262ab3cb968e460e95dd2ef
Svyazistvsa <sviazist.vsa@gmail.com>
2025-03-15 20:42:11 +0500

    Zadolbali procrutka i popstate

--- scripts/main_data.js
+++ scripts/main_data.js
@@ -3,8 +3,7 @@
 history.scrollRestoration = "manual";
 
 let main = document.getElementsByTagName("main")[0],
-    subMenu = document.querySelector("#subMenu"),
-    scrollPosition = undefined;
+    subMenu = document.querySelector("#subMenu");
 
 window.addEventListener("popstate", async(e) => {
     if (e.state) {
@@ -14,33 +13,25 @@ window.addEventListener("popstate", async(e) => {
                 break;
             default:               
                 await contentF(e.state.name);
-
-        }
-        
+        }        
     }
-    scrollP();
-    })
+    const { x, y } = e.state.scroll;
+    window.scrollTo(x, y);
+});
 
 document.addEventListener("pointerdown", async (e) => {   
     if(e.target.hasAttribute("data-name")){
         let name = e.target.dataset.name;
-        //scrollPosition = { x: window.scrollX, y: window.scrollY };
-        history.pushState({name: cp(), scroll: { x: window.scrollX, y: window.scrollY }, path:window.location.pathname }, "", window.location.pathname);        
-        await contentF(name);
-        
-        
-        window.scrollTo(0, 0);
-        
+        history.pushState({ name: cp(), scroll: { x: window.scrollX, y: window.scrollY }, path:window.location.pathname }, "", window.location.pathname);        
+        await contentF(name);        
+        window.scrollTo(0, 0);        
     }
     if(e.target.classList.contains("main_content")){
-        //scrollPosition = { x: window.scrollX, y: window.scrollY };
-        history.pushState({name: cp(), scroll: { x: window.scrollX, y: window.scrollY }, path:window.location.pathname},"", window.location.pathname);
-        await res();
-        
-        
+        history.pushState({ name: cp(), scroll: { x: window.scrollX, y: window.scrollY }, path:window.location.pathname }, "", window.location.pathname);
+        await res();        
         window.scrollTo(0, 0);
     }
-})
+});
 
 let contentF = async (name) => {
     let response = await fetch("/content", {
@@ -52,9 +43,7 @@ let contentF = async (name) => {
     if (response.ok) {
         const newDocument = await response.text();
         main.innerHTML = newDocument;
-        history.pushState({name: name, scroll: { x: 0, y: 0 }, path: "/content/" + name + "/"}, "", "/content/" + name + "/");
-        //history.replaceState({name: name, scroll: {x: 0, y: 0}, path: "/content/" + name + "/"}, "", "/content/" + name + "/");
-        //scrollPosition = { x: 0, y: 0 };
+        history.replaceState({name: name, scroll: { x: window.scrollX, y: window.scrollY}, path: "/content/" + name + "/"}, "", "/content/" + name + "/");
         if(subMenu.classList.contains("hidden")) subMenu.classList.remove("hidden");
         document.dispatchEvent(new CustomEvent("newContent", {bubbles:true}));        
     } else {
@@ -63,7 +52,7 @@ let contentF = async (name) => {
 }
 
 let res = async () =>{
-    let response = await fetch(`${cp()}?main=main`, {
+    let response = await fetch(`/?main=main`, {
         method: 'GET',
         headers: { "Content-Type": "application/json" },
     });
@@ -72,10 +61,8 @@ let res = async () =>{
         main.innerHTML = "";
         content.forEach((item) => {
             main.innerHTML +=item;
-        })
-        history.pushState({name: "main", scroll: { x: 0, y: 0 }, path: "/"}, "", "/");
-       // scrollPosition = {x: 0, y: 0 };
-        //history.replaceState({name: "main", scroll: {x: 0, y: 0}, path: "/"}, "", "/");
+        });
+        history.replaceState({name: "main", scroll: { x: window.scrollX, y: window.scrollY}, path: "/"}, "", "/");
         subMenu.classList.add("hidden");
         if(document.querySelector(".subUl")) {document.querySelector(".subUl").classList.add("hidden")};
     } else {
@@ -87,11 +74,11 @@ let scrollP = (e) => {
     const state = history.state || {};
     
   if (state.scroll &&  state.scroll.y !== 0) {
-    alert(state.scroll.y)
+    alert(state.scroll.y);
    const { x, y } = state.scroll;
    window.scrollTo(x, y);
   } if (e == 'zero'){
-    alert('zero')
+    alert('zero');
    window.scrollTo(0, 0);
   }
 };
@@ -101,7 +88,7 @@ let cp = () => {
         name,
         parts = currentPath.split('/');
     if (currentPath.startsWith('/content/') && parts.length >= 3 && parts[2] !== "") {
-            name = parts[2];            
+        name = parts[2];            
     } else {    
         name = "/";
     }
