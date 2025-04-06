4c96964ad2f84d481f5ee0d6ff2d90ad832ee273
Svyazistvsa <sviazist.vsa@gmail.com>
2025-04-06 19:26:57 +0500

    tablet is work

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const fs = require('fs').promises;
 const fsSync = require('fs');
 const cheerio = require('cheerio');
 const app = express();
-const port = 443;
+const port = 3000;
 
 let base = 'main_d.css',
     main_arr = [`<li class="out main_content">Главная страница</li>`],
@@ -43,9 +43,9 @@ async function readContentDirectory() {
 readContentDirectory();
 
 let choiceHtml = (req) =>{
-    let isMobile = req.useragent.isMobile;
-    console.log(req.useragent.isMobile);
-    if(isMobile){
+    if(req.useragent.isMobile){
+        base = 'mobile/index.html';
+    } else if (req.useragent.isTablet){
         base = 'mobile/index.html';
     } else {
         base = 'desctop/index.html';
