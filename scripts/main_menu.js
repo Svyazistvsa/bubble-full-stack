168e7d63987500508aaa96a783c6f59fc4224de2
Svyazistvsa <sviazist.vsa@gmail.com>
2025-08-14 16:37:32 +0500

    root in adress

--- scripts/main_menu.js
+++ scripts/main_menu.js
@@ -44,7 +44,7 @@ async function an_in() {
     });
 
     async function loadMenu() {
-        let response = await fetch('https://my-bubble.ru?menu=menu', {
+        let response = await fetch('/?menu=menu', {
             method: 'GET',
             headers: { "Content-Type": "application/json" },
         });
